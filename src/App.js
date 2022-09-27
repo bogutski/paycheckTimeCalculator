b60f065da08eb8df85f3df2dbb2204a0916ef957
Viktor Bogutskii <info@siteograf.com>
2022-09-26 21:58:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
+// calculate time interval duration in minutes from start to end, 24h format
+// start example '09:00'
+// end example '18:00'
+function calculateDuration(start, end) {
+  const startHours = parseInt(start.split(':')[0]);
+  const startMinutes = parseInt(start.split(':')[1]);
+  const endHours = parseInt(end.split(':')[0]);
+  const endMinutes = parseInt(end.split(':')[1]);
+
+  const totalStartMinutes = startHours * 60 + startMinutes;
+  const totalEndMinutes = endHours * 60 + endMinutes;
+
+  return totalEndMinutes - totalStartMinutes;
+}
+
+
+// calculate cost for minutes and rate per hour
+function calculateCost(min, rate) {
+  return (min / 60) * rate;
+}
+
+// format hours and minutes from minutes
+function formatHoursAndMinutes(min) {
+  const hours = Math.floor(min / 60);
+  const minutes = min % 60;
+
+  return `${hours} hours ${minutes} min`;
+}
+
+// app for calculation work time and earned money
 function App() {
+
+
+  // state for rate
+  const [rate, setRate] = useState(15);
+
+  // state for slot hours array
+  // slot example
+  // {
+  //   id: 1,
+  //   start: '09:00',
+  //   end: '10:00',
+  //   totalMin: 60,
+  //   totalCost: 1,
+  //  }
+
+  const [slots, setSlots] = useState([]);
+
+  // onSlotChange
+  const onSlotChangeStart = (id, start) => {
+    const newSlots = slots.map((slot) => {
+        if (slot.id === id) {
+          const totalMin = calculateDuration(start, slot.end);
+          return {
+            ...slot,
+            start,
+            totalMin,
+            totalCost: calculateCost(totalMin, rate),
+          };
+        }
+        return slot;
+      }
+    );
+
+    setSlots(newSlots);
+  };
+
+  const onSlotChangeEnd = (id, end) => {
+    const newSlots = slots.map((slot) => {
+      if (slot.id === id) {
+        const totalMin = calculateDuration(slot.start, end);
+        return {
+          ...slot,
+          end,
+          totalMin,
+          totalCost: calculateCost(totalMin, rate),
+        };
+      }
+      return slot;
+    });
+
+    setSlots(newSlots);
+  };
+
+  const onAddSlot = () => {
+    const updatedSlots = [...slots, {
+      id: slots.length + 1,
+      start: '',
+      end: '',
+      totalMin: 0,
+      totalCost: 0,
+    }];
+
+    setSlots(updatedSlots);
+  };
+
+  const onRemoveSlot = (id) => {
+    const updatedSlots = slots.filter((slot) => slot.id !== id);
+    setSlots(updatedSlots);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Paycheck Calculation</h4>
+
+      <div>
+        <label>Rate per hour, $</label>
+        <input type="number" value={rate}
+               onChange={(e) => setRate(e.target.value)} />
+      </div>
+
+      <hr />
+
+      <div>
+        {slots.map((slot) => (
+          <div key={slot.id}>
+            <label>Start</label>
+            <input type="time" value={slot.start}
+                   onChange={(e) => onSlotChangeStart(slot.id, e.target.value)} />
+
+            <label>End</label>
+            <input type="time" value={slot.end}
+                   onChange={(e) => onSlotChangeEnd(slot.id, e.target.value)} />
+
+            <button onClick={() => onRemoveSlot(slot.id)}>Remove</button>
+
+
+            <span>{slot.totalMin} min</span>
+
+            <span>${slot.totalCost}</span>
+          </div>
+        ))}
+      </div>
+
+
+      <button onClick={onAddSlot}>Add slot</button>
+
+      Total time and cost $
+      <div>
+        {formatHoursAndMinutes(slots.reduce((acc, slot) => acc + slot.totalMin, 0))}
+
+        $ {slots.reduce((acc, slot) => acc + slot.totalCost, 0)}
+
+      </div>
+
+
+
+      <hr />
+
+      <pre>
+        {JSON.stringify(slots, null, 2)}
+      </pre>
     </div>
   );
 }
